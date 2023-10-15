b27e0b3e7f121a4c1ee3e4828434756536660363
Fendy <zf@fendy5.cn>
2023-10-15 23:31:38 +0800

    build: 配置开发代理

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   // distDir: 'build'
   reactStrictMode: true,
   transpilePackages: ['antd'],
-  output: 'standalone'
+  output: 'standalone',
+  async rewrites() {
+    return [
+      {
+        source: '/dev-api/:path*',
+        destination: `http://localhost:8000/:path*` // 将请求转发到目标服务器
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
