f669ccce43b78c824befcac64f058597a33c3e4e
Fendy <zf@fendy5.cn>
2023-10-15 23:15:54 +0800

    perf: 防止首次加载页面抖动

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,11 @@
 import './globals.css'
 import '@/styles/antd.scss'
 import { Inter } from 'next/font/google'
+// import Provider from '@/context/client-provider'
+// import { authOptions } from '@/app/api/auth/[...nextauth]/route'
 import React from 'react'
+// import { getServerSession } from 'next-auth'
+import StyledComponentsRegistry from '@/lib/AntdRegistry'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,10 +14,13 @@ export const metadata = {
   description: '随机字符串 图片格式转换 图片批量压缩 二进制转换'
 }
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  // const session = await getServerSession(authOptions)
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
     </html>
   )
 }
